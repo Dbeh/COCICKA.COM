27ff31555723e78a220f884b048ac8c2b885a6b2
viktorshcherbyna <viktorshcherbyna1705@gmail.com>
2019-06-24 19:15:37 +0300

    less 9. 0.1

--- home.php
+++ home.php
@@ -8,6 +8,12 @@
 <ul class="nav">
     <li><a href="homework/1/index.php">1</a></li>  |
     <li><a href="homework/2/index.php">2</a></li>  |
+    <li><a href="homework/3/index.php">3</a></li>  |
+    <li><a href="homework/4/index.php">4</a></li>  |
+    <li><a href="homework/5/index.php">5</a></li>  |
+    <li><a href="homework/6/index.php">6</a></li>  |
+    <li><a href="homework/7/index.php">7</a></li>  |
+    <li><a href="homework/8/index.php">8</a></li>  |
 </ul>
 
 </body>
